a263375cd6b7beeb2e649382ff91ae5fd52c82f6
ad5915 <ad5915@imperial.ac.uk>
2016-02-23 23:24:22 +0000

    Added dynamic content for basic menu.

--- navigator.js
+++ navigator.js
@@ -1,7 +1,19 @@
 function activator(element) {
-	if (document.getElementById(element).className == 'active') {
-		document.getElementById(element).className = '';
-	} else {
-		document.getElementById(element).className = 'active';
+	document.getElementById(element).className = 'active';
+}
+
+function deactivator(element, suffix) {
+	var elements = [];
+	for (var i = 0; i < suffix.length; ++i) {
+		elements.push(element.concat(suffix[i]));
+	} 
+	for (var i = 0; i < elements.length; ++i) {
+		document.getElementById(elements[i]).className = '';
 	}
 }
+
+function changeTitle(name) { // dependency on index.js
+	document.title = name;
+	copyFrame(frameMap[name], 'content');
+}
+
