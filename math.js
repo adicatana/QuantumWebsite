bb13fc50fe4d29dd2275b5683569743e13984748
Adrian Catana <adrian.catana15@imperial.ac.uk>
2016-03-10 01:40:05 +0000

    Modified all latex

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ var formulas = (function() {
 	return {
 		formula1 : "\\log_2 N",
 		formula2 : "\\(x, y) + (z, t) = (x + z, y + t)",
-		formula3 : "\\(x, y) (z, t) = (xz - yt, xy + yz)",
+		formula3 : "\\(x, y) \\cdot (z, t) = (xz - yt, xy + yz)",
 		formula4 : "\\OM = \sqrt{(x_M - x_O)^2 + (y_M - y_O)^2}",
 		formula5 : "A = " + matrix(3,3,["a_{11}","...","a_{1n}","...","...","...","a_{m1}","...","a_{mn}"]) + " = [a_{ij}]",
 		formula6 : "I_n = " + matrix(4, 4,[1, 0, "...", 0, 0, 1, "...", 0, "...", "...", "...", "...", 0, 0, "...", 1]),
