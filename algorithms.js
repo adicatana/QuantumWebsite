aa0df0ac6fe66e07ac0c6616043b82f1add1c851
ad5915 <ad5915@imperial.ac.uk>
2016-03-11 19:45:09 +0000

    Finished Grover's.

--- algorithms.js
+++ algorithms.js
@@ -22,6 +22,10 @@ var algoFormulas = (function() {
 						  "\\frac{1}{\\sqrt{2}}","-\\frac{1}{\\sqrt{2}}"])
 	};
 
+	var partial = "\\bigg[ \\frac{" + bit(0) + "-" + bit(1) + "}{\\sqrt{2}} \\bigg] ";
+	var u2 = "\\frac {1} {2^{n}}";
+	var u3 = "\\frac {2} {2^{n}}";
+
 	return {
 		fdef : halign(
 			"f : \\{0, 1\\} \\rightarrow \\{0, 1\\}",
@@ -46,17 +50,32 @@ var algoFormulas = (function() {
 		gg3 : bit("\\phi_{0}") + "=" + bit("0,0"),
 		gg4 : bit("\\phi_{1}") + "=" + "\\bigg[ \\frac{ \\sum_{x \\in \\{ 0, \\ 1 \\}^{n} } " + bit("x") + "}{\\sqrt{2^{n}}} \\bigg]" + bit("0,0"),
 		gg5 : bit("\\phi_{2}") + "=" + " \\frac{ \\sum_{x \\in \\{ 0, \\ 1 \\}^{n} } " + bit("x, f(x)") + "}{\\sqrt{2^{n}}} ",
-		gg6 : "",
-		gg7 : "",
-		gg8 : "",
-		gg9 : "",
-		gg10 : "",
+		gg6 : bit("\\phi_{0}") + "=" + bit("x,1"),
+		gg7 : bit("\\phi_{1}") + "=" + bit("x") + "\\bigg[  \\frac{" + bit(0) + "-" + bit(1) + "}{\\sqrt{2}} \\bigg] " 
+			+ "= \\bigg[  \\frac{" + bit("x,0") + "-" + bit("x,1") + "}{\\sqrt{2}} \\bigg] ",
+		gg8 : bit("\\phi_{2}") + "= (-1)^{f(x)}" + bit("x") + partial + "= -"+ bit("x") + partial 
+			+ ",if \\ x = x_{0}",
+		gg9 : bit("\\phi_{2}") + "= (-1)^{f(x)}" + bit("x") + partial + "= +"+ bit("x") + partial 
+			+ ",if \\ x \\neq x_{0}",
+		gg10 : "A =" + matrix(4,4,[u2,u2,"...",u2,
+								   u2,u2,"...",u2,
+								   "...","...","...","...",
+								   u2,u2,"...",u2]),
+		gg11 : "-I + 2A =" + matrix(4,4,["-1+"+u3,u3,"...",u3,
+								         u3,"-1+"+u3,"...",u3,
+								         "...","...","...","...",
+								         u3,u3,"...","-1+"+u3]),
 
 		ini1 : "\\sqrt{m}",
 		ini2 : "x_{0}",
 		ini3 : bit("0,x") + "\\  to \\ " + bit("x, f(x) \\oplus y"),
-		ini4 : "",
-		ini5 : "",
+		ini4 : "\\frac{" + bit(0) + "+" + bit(1) + "}{\\sqrt{2}}",
+		ini5 : "U_{f}(I \\oplus H)" + bit("x, 1"),
+		ini6 : "U_{f}",
+		ini7 : "U_{f}",
+		ini8 : bit(0),
+		ini9 : "H^{\\otimes n}",
+		ini10 : "\\sqrt{m}(or \\ \\sqrt{2^{n}})",
 
 		system : "A \\cdot \\vec{x} = \\vec{b}",	
 		system2 : "\\vec{x} = A^{-1} \\cdot \\vec{b}",
